7784b6604d7b76e0c2e3a932ec152d641d293d90
Divyanshu Pathania <divyanshu.990pathania@gmail.com>
2023-08-22 10:54:21 +0530

    Setup light and dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@emotion/react';
+
+import { THEME_TYPES } from 'constant';
+import { Themes } from 'constant/theme';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const [currentTheme, setCurrentTheme] = useState<string>(THEME_TYPES.dark);
+
+	useEffect(() => {
+		setCurrentTheme(localStorage.getItem('theme') || THEME_TYPES.dark);
+	}, []);
+
+	return (
+		<ThemeProvider theme={Themes[currentTheme]}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
