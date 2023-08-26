48f535677f8c0039ff1f5cd412b89dca5a6186f7
Divyanshu Pathania <divyanshu.990pathania@gmail.com>
2023-08-27 02:14:55 +0530

    Modify badge component css

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -14,7 +14,7 @@ const Badge: React.FC<BadgeProps> = (props: BadgeProps) => {
 	const { label, type, style } = props;
 	return (
 		<Container backgroundColor={colorMap[type]} style={style}>
-			<Typography type="l6" shade="strong">
+			<Typography type="l6" shade="strong" style={{ color: colorMap[type] }}>
 				{label}
 			</Typography>
 		</Container>
--- components/Badge/style.ts
+++ components/Badge/style.ts
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div<{ backgroundColor: string}>`
-	border: ${(props) => `2px ${props.backgroundColor}`};
+	border: ${(props) => `2px solid ${props.backgroundColor}`};
 	padding: 4px 12px;
-	background: ${(props) => props.backgroundColor};
 	border-radius: 8px;
 	display: flex;
 	justify-content: center;
